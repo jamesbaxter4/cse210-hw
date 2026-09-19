39bf3bf24be6691d1a602b8da22dce0ffa50965a
J Baxter <jamesvbaxter4@gmail.com>
2026-09-18 22:19:11 -0700

    finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,19 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask first name
+        Console.WriteLine("What is your first name?");
+
+        string first_name = Console.ReadLine();
+
+        // Ask last name
+        Console.WriteLine("What is your last name?");
+
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
+
     }
 }
